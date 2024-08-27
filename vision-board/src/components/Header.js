81fd28b10f4a19b265f1fb3c1ff25a0f88fd9a51
Daniel Giathi <blacksharkchi@proton.me>
2024-08-27 12:08:28 +0300

    update header

---
+++ vision-board/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+        <header className="bg-white shadow-md p-4 rounded-md mb-6">
+        <h1 className="text-3xl font-bold text-gray-800 text-center">My Vision Board</h1>
+      </header>
+  )
+}
+
+export default Header
