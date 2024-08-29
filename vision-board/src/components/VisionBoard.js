3814513e0725e10353902e25a3197619a3a7288a
Daniel Giathi <blacksharkchi@proton.me>
2024-08-29 22:33:18 +0300

    remove link import

--- vision-board/src/components/VisionBoard.js
+++ vision-board/src/components/VisionBoard.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import {toast} from 'react-toastify'
 import AuthContext from './AuthContext';
 
