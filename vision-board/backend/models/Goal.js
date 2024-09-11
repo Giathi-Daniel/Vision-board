8e77df48fb4679efbce6e259a241f9c69d940a46
Daniel Giathi <blacksharkchi@proton.me>
2024-09-11 06:59:11 +0300

    refine Goal model

--- vision-board/backend/models/Goal.js
+++ vision-board/backend/models/Goal.js
@@ -4,27 +4,24 @@ const subgoalSchema = new mongoose.Schema({
     title: {type: String, required: true},
     description: {type: String},
     completed: {type: Boolean, default: false},
-    progress: {type: Number, default: 0}
+    progress: {type: Number, default: 0, min: 0, max: 100}
 })
 
 const goalSchema = new mongoose.Schema({
     title: {type: String, required: true},
-    description: {type: String},
-    reminderDate: Date,
-    category: {type: String},
-    completionPercentage: {type: Number, default: 0},
+    description: {type: String, default: ''},
+    reminderDate: {type: Date, default: null},
+    category: {type: String, default: 'General'},
+    completionPercentage: {type: Number, default: 0, min: 0, max: 100},
     subgoals: [subgoalSchema],
     sharedWith: { type: [String], default: [] },
     isPublic: { type: Boolean, default: false },
-    shareableLink: { type: String }, 
     createdAt: { type: Date, default: Date.now }
 })
 
-goalSchema.methods.generateShareableLink = function () {
-    const baseUrl = 'https://vision-board.vercel/share/';
-    const link = baseUrl + this._id;
-    return link;
-}
+goalSchema.virtual('sharebleLink').get(function() {
+    return `https:vision-board.vercel.app/share/${this._id}`;
+})
 
 const Goal = mongoose.model('Goal', goalSchema)
 
