72e812cc3be5b33c50190c4fcbd8c0270dfd38e8
Daniel Giathi <blacksharkchi@proton.me>
2024-09-11 06:55:48 +0300

    update middleware

--- vision-board/backend/middleware/authMiddleware.js
+++ vision-board/backend/middleware/authMiddleware.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken')
 
 const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization')
+    const token = req.header('Authorization')?.split(' ')[1]
 
     if(!token) {
         return res.status(401).json({ message: 'No token found, authorization denied'})
     }
 
     try {
-        const decoded = jwt.verify(token, 'jwtSecret')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         req.userId = decoded.userId
         next()
     } catch (err) {
-        res.status(401).json({ message: err.message })
+        res.status(401).json({ message: 'Invalid token, authorization denied' })
     }
     
 }
